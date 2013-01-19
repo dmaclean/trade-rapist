6265bb43322d94231e85de7c5b6e9c150f502842
Dan MacLean <dmaclean82@gmail.com>
2013-01-18 22:50:04 -0500

    Configured dev environment to use the MySQL database.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -13,8 +13,13 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate=""
+            url = "jdbc:mysql://localhost/fantasy_yahoo"
+            driverClassName = "com.mysql.jdbc.Driver"
+            username = "fantasy"
+            password = "fantasy"
         }
     }
     test {
