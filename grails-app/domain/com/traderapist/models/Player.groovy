fcf96efc27094f076dbb347b8145167c7675183f
Dan MacLean <dmaclean82@gmail.com>
2013-01-20 00:33:29 -0500

    Fixed the declaration of transient attributes, correctly declared the stat maps as type Map, and added the afterLoad() hook.

--- grails-app/domain/com/traderapist/models/Player.groovy
+++ grails-app/domain/com/traderapist/models/Player.groovy
@@ -3,8 +3,15 @@ package com.traderapist.models
 class Player {
     static hasMany = [stats: Stat]
 
+    static transients = ['seasonStats', 'seasonStatPoints', 'weeklyStats', 'weeklyStatPoints']
+
     String name
     String position
+    Map seasonStats = [:]
+    Map seasonStatPoints = [:]
+    Map weeklyStats = [:]
+    Map weeklyStatPoints = [:]
+
     static constraints = {
         name blank:false
         position blank:false
@@ -13,5 +20,35 @@ class Player {
     static mapping = {
         table "players"
         version false
+    }
+
+    def afterLoad() {
+        for(s in stats) {
+            def seasonStr = String.valueOf(s.season)
+            def weekStr = String.valueOf(s.week)
+
+            // Stat is season
+            if(s.week == -1) {
+                if(!seasonStats[seasonStr]) {
+                    seasonStats[seasonStr] = []
+                }
+                seasonStats[seasonStr] << s
+            }
+            //Stat is week
+            else {
+                // Season entry doesn't exist yet --> create map for it
+                if(!weeklyStats[seasonStr]) {
+                    weeklyStats[seasonStr] = [weekStr: [s]]
+                }
+                // Season exists, but week doesn't --> create map for it
+                else if(!weeklyStats[seasonStr][weekStr]) {
+                    weeklyStats[seasonStr][weekStr] = [s]
+                }
+                // Both already exist, just append the stat
+                else {
+                    weeklyStats[seasonStr][weekStr] << s
+                }
+            }
+        }
     }
 }
--- test/unit/com/traderapist/models/PlayerTests.groovy
+++ test/unit/com/traderapist/models/PlayerTests.groovy
@@ -9,6 +9,7 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Player)
+@Mock([Stat])
 class PlayerTests {
 
    void testNameNotBlank() {
@@ -30,4 +31,61 @@ class PlayerTests {
 
         assert "blank" == player.errors["position"]
     }
+
+//    void testAfterLoad_Season() {
+//        def player = new Player(name: "Dan MacLean", position: "QB")
+//        assertEquals 0, player.seasonStats.size()
+//        assertEquals 0, player.seasonStatPoints.size()
+//        assertEquals 0, player.weeklyStats.size()
+//        assertEquals 0, player.weeklyStatPoints.size()
+//        player.save(flush:true)
+//
+//        def stat = new Stat(player: player, season: 2001, week: -1, statKey: 4, statValue: 100)
+//        stat.save(flush: true)
+//
+//        def player2 = Player.get(player.id)
+//        assertEquals 1, player2.stats.size()
+//
+//        /*
+//        Should have 1 season stat for 2001
+//         */
+//        assertTrue player2.seasonStats.size() == 1
+//        assertTrue player2.seasonStats.containsKey("2001")
+//        def stats = player.seasonStats["2001"]
+//        assertEquals stats[0].id, stat.id
+//
+//        /*
+//        Should contain no week stats
+//         */
+//        assertEquals 0, player.weeklyStats.size()
+//        assertEquals 0, player.weeklyStatPoints.size()
+//    }
+//
+//    void testAfterLoad_Weekly() {
+//        def player = new Player(name: "Dan MacLean", position: "QB")
+//        assertEquals 0, player.seasonStats.size()
+//        assertEquals 0, player.seasonStatPoints.size()
+//        assertEquals 0, player.weeklyStats.size()
+//        assertEquals 0, player.weeklyStatPoints.size()
+//        player.save(flush:true)
+//
+//        def stat = new Stat(player: player, season: 2001, week: 1, statKey: 4, statValue: 100)
+//        stat.save(flush: true)
+//
+//        def player2 = Player.get(player.id)
+//        assertEquals 1, player2.stats.size()
+//
+//        /*
+//        Should have no season stats
+//         */
+//        assertEquals 0, player.seasonStats.size()
+//        assertEquals 0, player.seasonStatPoints.size()
+//
+//        /*
+//        Weekly stats should contain a map for 2001, which contains an arraylist for week 1
+//         */
+//        assertTrue player.weeklyStats.containsKey("2001")
+//        def week1 = player.weeklyStats["2001"]["1"]
+//        assertEquals week1[0].id, stat.id
+//    }
 }
