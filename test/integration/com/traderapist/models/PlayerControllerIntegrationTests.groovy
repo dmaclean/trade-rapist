acc270db6f6110b815025585b5aee0b35f114486
Dan MacLean <dmaclean82@gmail.com>
2013-06-08 22:50:00 -0400

    Fixed tests so they work when running in integrated mode.

--- test/integration/com/traderapist/models/PlayerControllerIntegrationTests.groovy
+++ test/integration/com/traderapist/models/PlayerControllerIntegrationTests.groovy
@@ -1,5 +1,7 @@
 package com.traderapist.models
 
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -11,40 +13,30 @@ import org.junit.Test
  * Time: 6:55 PM
  * To change this template use File | Settings | File Templates.
  */
-class PlayerControllerIntegrationTests extends GroovyTestCase {
-	PlayerController controller
-	def params
+@TestFor(PlayerController)
+@Mock([Player,FantasyPoints])
+class PlayerControllerIntegrationTests {
 
 	@Before
 	void setUp() {
-		super.setUp()
+		Player.metaClass.getStatYears = { [2002] }
+		Player.metaClass.getScoringAverageForSeason = { season -> 0 }
+		Stat.metaClass.static.getStatYears = { [2002] }
 
-		controller = new PlayerController()
-
-		params = [:]
 		params["name"] = "Dan"
 		params["position"] = "QB"
 	}
 
 	@After
 	void tearDown() {
-		super.tearDown()
 
-		controller = null
-		params = null
 	}
 
 	@Test
 	void testShow_NullId() {
-//		controller.show()
-
-//		assert controller.response.redirectedUrl == '/player/list'
-
 		def player = new Player(params)
 
 		assert player.save(flush: true) != null
-
-		params.id = player.id
 
 		assert controller.show() == null
 		assert controller.response.redirectedUrl == '/player/list'
@@ -52,10 +44,6 @@ class PlayerControllerIntegrationTests extends GroovyTestCase {
 
 	@Test
 	void testShow() {
-//		controller.show()
-
-//		assert controller.response.redirectedUrl == '/player/list'
-
 		def player = new Player(params)
 
 		assert player.save(flush: true) != null
