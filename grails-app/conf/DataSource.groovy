d206efb3497ad6f9c6be6627a92db07096b79c5a
Dan MacLean <dmaclean82@gmail.com>
2013-08-11 22:14:56 -0400

    Turned 2nd level caching and query caching off.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -5,8 +5,8 @@ dataSource {
     password = ""
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
+    cache.use_second_level_cache = false
+    cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
@@ -20,7 +20,7 @@ environments {
             driverClassName = "com.mysql.jdbc.Driver"
             username = "fantasy"
             password = "fantasy"
-            logSql=true
+            logSql=false
         }
     }
     test {
