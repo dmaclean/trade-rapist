6e9a33be3bae5a236d7f3ea4e3dc12f77c46d413
Dan MacLean <dmaclean82@gmail.com>
2013-08-18 10:40:31 -0400

    Added Amazon RDS database to production datasource.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -32,19 +32,24 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+//            dbCreate = "update"
+//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//            pooled = true
+//            properties {
+//               maxActive = -1
+//               minEvictableIdleTimeMillis=1800000
+//               timeBetweenEvictionRunsMillis=1800000
+//               numTestsPerEvictionRun=3
+//               testOnBorrow=true
+//               testWhileIdle=true
+//               testOnReturn=true
+//               validationQuery="SELECT 1"
+//            }
+	        url = "jdbc:mysql://traderapist.cz4klpvd9rfo.us-east-1.rds.amazonaws.com:3306"
+	        driverClassName = "com.mysql.jdbc.Driver"
+	        username = "traderapist"
+	        password = "santoro821"
+	        logSql=false
         }
     }
 }
