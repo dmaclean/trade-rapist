3af58e0c85fb0c7b382b9037eae8986c8b943257
Dan MacLean <dmaclean82@gmail.com>
2013-08-22 10:53:26 -0400

    Added a try/catch block to the FPJ scheduler so if one thing goes wrong the thread doesn't stop executing.

--- src/groovy/com/traderapist/automation/FantasyPointProjectionScheduler.groovy
+++ src/groovy/com/traderapist/automation/FantasyPointProjectionScheduler.groovy
@@ -21,31 +21,37 @@ class FantasyPointProjectionScheduler implements Runnable {
 
 		while(true) {
 			if(!FantasyPointsJobController.processing) {
-				/*
-				 * Fetch all Projection jobs
-				 */
-				def jobs = FantasyPointsJob.findAllByCompletedAndProjectionInList(false,
-						[FantasyPointsJob.TRADERAPIST_PROJECTION,
-						 FantasyPointsJob.YAHOO_PPR_PROJECTION,
-						 FantasyPointsJob.YAHOO_STANDARD_PROJECTION])
-				jobs.each {     job ->
-					long start = System.currentTimeMillis()
-					def link = grailsLinkGenerator.link(absolute: true, controller: "fantasyPointsJob", action: "process", params: [fantasy_points_job_id : job.id])
-					def resp = rest.get(link)
-					long end = System.currentTimeMillis()
-					println "FPJ job ${ job.id }/${ job.season }/${ job.week }/${ (job.projection) ? "proj" : "not_proj" } completed in ${ (end-start)/1000.0 }"
-				}
+				try {
+					/*
+					 * Fetch all Projection jobs
+					 */
+					def jobs = FantasyPointsJob.findAllByCompletedAndProjectionInList(false,
+							[FantasyPointsJob.TRADERAPIST_PROJECTION,
+							 FantasyPointsJob.YAHOO_PPR_PROJECTION,
+							 FantasyPointsJob.YAHOO_STANDARD_PROJECTION])
+					jobs.each {     job ->
+						long start = System.currentTimeMillis()
+						def link = grailsLinkGenerator.link(absolute: true, controller: "fantasyPointsJob", action: "process", params: [fantasy_points_job_id : job.id])
+						def resp = rest.get(link)
+						long end = System.currentTimeMillis()
+						println "FPJ job ${ job.id }/${ job.season }/${ job.week }/${ (job.projection) ? "proj" : "not_proj" } completed in ${ (end-start)/1000.0 }"
+					}
 
-				/*
-				 * Fetch all point generation jobs
-				 */
-				jobs = FantasyPointsJob.findAllByCompletedAndProjection(false, FantasyPointsJob.NO_PROJECTION)
-				jobs.each {     job ->
-                    long start = System.currentTimeMillis()
-                    def resp = rest.get(grailsLinkGenerator.link(absolute: true, controller: "fantasyPointsJob", action: "process", params: [fantasy_points_job_id : job.id]))
-                    long end = System.currentTimeMillis()
-                    println "FPJ job ${ job.id }/${ job.season }/${ job.week }/${ (job.projection) ? "proj" : "not_proj" } completed in ${ (end-start)/1000.0 }"
-                }
+					/*
+					 * Fetch all point generation jobs
+					 */
+					jobs = FantasyPointsJob.findAllByCompletedAndProjection(false, FantasyPointsJob.NO_PROJECTION)
+					jobs.each {     job ->
+	                    long start = System.currentTimeMillis()
+	                    def resp = rest.get(grailsLinkGenerator.link(absolute: true, controller: "fantasyPointsJob", action: "process", params: [fantasy_points_job_id : job.id]))
+	                    long end = System.currentTimeMillis()
+	                    println "FPJ job ${ job.id }/${ job.season }/${ job.week }/${ (job.projection) ? "proj" : "not_proj" } completed in ${ (end-start)/1000.0 }"
+	                }
+				}
+				catch(Exception e) {
+					println "${ e.getMessage() }"
+					e.printStackTrace()
+				}
             }
 
             Thread.sleep(1000*60*5)
